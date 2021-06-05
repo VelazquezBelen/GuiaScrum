4c2dc4e4b0c874c6d30c1fc8abfd2918a5baebe5
VelazquezBelen <velazquezbelenr@gmail.com>
2021-06-05 16:59:15 -0300

    heroku

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 url = 'https://scrumtalk.herokuapp.com/webhooks/rest/webhook' 
 myobj = {
-"message": "hi",
+"message": "hola",
 "sender": 1,
 }
 x = requests.post(url, json = myobj)
